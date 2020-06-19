4be318c905cb86713003bdd4b84d03403497ea10
luts83 <lee.sanggean@gmail.com>
2020-06-20 00:16:44 +0100

    00:16 | Jun20

--- my_site_prj/settings/base.py
+++ my_site_prj/settings/base.py
@@ -165,6 +165,6 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = secrets['EMAIL_HOST_USER'] #
-EMAIL_HOST_PASSWORD = secrets['EMAIL_HOST_PASSWORD'] #
+EMAIL_HOST_PASSWORD = secrets['EMAIL_HOST_PASSWORD']
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
