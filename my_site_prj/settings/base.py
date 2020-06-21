0551c2ddfd1817f9dd838696cbd9899e00a7e34f
luts83 <lee.sanggean@gmail.com>
2020-06-21 18:22:23 +0100

    18:21|21Jun20

--- my_site_prj/settings/base.py
+++ my_site_prj/settings/base.py
@@ -93,12 +93,26 @@ WSGI_APPLICATION = 'my_site_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'eduocean', # 데이터베이스 이름
+#         'USER': 'root', # 접속 사용자 이름
+#         'PASSWORD': '123456789', # 접속 비밀번호
+#         'HOST': 'localhost',
+#         'PORT': '3306', # 기본 포트
+#         'OPTIONS': {
+#             'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+#         }
+#     }
+# }
 
 
 # Password validation
--- my_site_prj/settings/production.py
+++ my_site_prj/settings/production.py
@@ -1,9 +1,23 @@
 from .base import *
 DEBUG = True
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eduocean', # 데이터베이스 이름
+        'USER': 'eduocean', # 접속 사용자 이름
+        'PASSWORD': 'roots1983', # 접속 비밀번호
+        'HOST': 'eduocean.cadcrhj7dhqo.us-east-2.rds.amazonaws.com',
+        'PORT': '3306', # 기본 포트
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
