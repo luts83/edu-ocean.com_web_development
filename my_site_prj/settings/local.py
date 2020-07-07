168222d6a263f934042bda20d87ef1a9db6bcce8
luts83 <lee.sanggean@gmail.com>
2020-07-07 08:40:18 +0100

    08:40|07July20

--- my_site_prj/settings/local.py
+++ my_site_prj/settings/local.py
@@ -17,5 +17,9 @@ DATABASES = {
         'PASSWORD': '123456789',  # 접속 비밀번호
         'HOST': 'localhost',
         'PORT': '3306',  # 기본 포트
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4'
+        }
     }
 }
--- my_site_prj/settings/production.py
+++ my_site_prj/settings/production.py
@@ -15,7 +15,8 @@ DATABASES = {
         'HOST': secrets['DATABASES_HOST'],
         'PORT': '3306',
         'OPTIONS': {
-            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset' : 'utf8mb4'
         }
     }
 }
